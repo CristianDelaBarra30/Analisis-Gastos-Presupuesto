92328ce494bbfcfd0de05fdc85f8575db186df92
Cristian De La Barra <cdelabd21@gmail.com>
2026-04-01 01:03:24 -0300

    Update 02_vs_presupuesto.sql

--- sql/02_vs_presupuesto.sql
+++ sql/02_vs_presupuesto.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+    g.categoria,
+    SUM(g.monto) AS gasto_total,
+    MAX(p.presupuesto_mensual) AS presupuesto,
+    SUM(g.monto) - MAX(p.presupuesto_mensual) AS diferencia
+FROM gastos g
+JOIN presupuesto p 
+    ON g.categoria = p.categoria
+GROUP BY g.categoria
+ORDER BY diferencia DESC;
