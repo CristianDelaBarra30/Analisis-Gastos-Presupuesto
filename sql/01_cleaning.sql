eb084d87326583eb2edd9dd616b9f7816939b455
Cristian De La Barra <cdelabd21@gmail.com>
2026-04-02 01:57:26 -0300

    Update 01_cleaning.sql

--- sql/01_cleaning.sql
+++ sql/01_cleaning.sql
@@ -1,4 +1,4 @@
---Cleaning tabla centro costo
+-- Cleaning gastos
 SELECT
     TRIM(LOWER(categoria)) AS categoria,
     REPLACE(TRIM(LOWER(centro_costo)),'-','') AS centro_costo,
@@ -6,10 +6,10 @@ SELECT
     fecha
 FROM gastos;
 
---Cleaning tabla presupuestos
+-- Cleaning presupuesto
 SELECT
     TRIM(LOWER(categoria)) AS categoria,
-    TRIM(LOWER(centro_costo)) AS centro_costo,
+    REPLACE(TRIM(LOWER(centro_costo)),'-','') AS centro_costo,
     CAST(monto_presupuestado AS DECIMAL(18,2)) AS monto_presupuestado,
     fecha,
     version_presupuesto
