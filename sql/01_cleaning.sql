beb6259bb30ac2e6c4f632bbbf842a97a8a4b107
Cristian De La Barra <cdelabd21@gmail.com>
2026-04-02 01:00:18 -0300

    Update 01_cleaning.sql

--- sql/01_cleaning.sql
+++ sql/01_cleaning.sql
@@ -1,1 +1,7 @@
-
+SELECT
+    TRIM(categoria) AS categoria,
+    TRIM(centro_costo) AS centro_costo,
+    CAST(monto_real AS FLOAT) AS monto_real,
+    CAST(monto_presupuestado AS FLOAT) AS monto_presupuestado,
+    fecha
+FROM gastos;
