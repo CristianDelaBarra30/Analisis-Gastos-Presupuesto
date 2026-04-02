7ce058968f0691bac8e375c2155a62529f0bd070
Cristian De La Barra <cdelabd21@gmail.com>
2026-04-02 01:59:41 -0300

    Update 05_gastos_vs_presupuesto.sql

--- sql/05_gastos_vs_presupuesto.sql
+++ sql/05_gastos_vs_presupuesto.sql
@@ -1,31 +1,31 @@
 WITH gastos_clean AS (
     SELECT
         TRIM(LOWER(categoria)) AS categoria,
-        TRIM(LOWER(centro_costo)) AS centro_costo,
-        fecha,
-        monto
+        REPLACE(TRIM(LOWER(centro_costo)),'-','') AS centro_costo,
+        DATE_TRUNC('month', fecha) AS mes,
+        CAST(monto AS DECIMAL(18,2)) AS monto
     FROM gastos
 ),
 presupuesto_clean AS (
     SELECT
         TRIM(LOWER(categoria)) AS categoria,
-        TRIM(LOWER(centro_costo)) AS centro_costo,
-        fecha,
-        monto_presupuestado
+        REPLACE(TRIM(LOWER(centro_costo)),'-','') AS centro_costo,
+        DATE_TRUNC('month', fecha) AS mes,
+        CAST(monto_presupuestado AS DECIMAL(18,2)) AS monto_presupuestado
     FROM presupuesto
 )
 
 SELECT
     g.categoria,
     g.centro_costo,
-    DATE_TRUNC('month', g.fecha) AS mes,
+    g.mes,
     SUM(g.monto) AS gasto_total,
-    SUM(p.monto_presupuestado) AS presupuesto,
-    SUM(g.monto) - SUM(p.monto_presupuestado) AS diferencia
+    COALESCE(SUM(p.monto_presupuestado),0) AS presupuesto,
+    SUM(g.monto) - COALESCE(SUM(p.monto_presupuestado),0) AS diferencia
 FROM gastos_clean g
 LEFT JOIN presupuesto_clean p
     ON g.categoria = p.categoria
     AND g.centro_costo = p.centro_costo
-    AND DATE_TRUNC('month', g.fecha) = DATE_TRUNC('month', p.fecha)
+    AND g.mes = p.mes
 GROUP BY 1,2,3
 ORDER BY diferencia DESC;
