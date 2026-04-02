70e6d09f9be77c9edb93a4c444038456e97386f7
Cristian De La Barra <cdelabd21@gmail.com>
2026-04-02 01:27:41 -0300

    Update 01_cleaning.sql

--- sql/01_cleaning.sql
+++ sql/01_cleaning.sql
@@ -1,7 +1,16 @@
+--Cleaning tabla centro costo
 SELECT
-    TRIM(categoria) AS categoria,
-    TRIM(centro_costo) AS centro_costo,
-    CAST(monto_real AS FLOAT) AS monto_real,
-    CAST(monto_presupuestado AS FLOAT) AS monto_presupuestado,
+    TRIM(LOWER(categoria)) AS categoria,
+    TRIM(LOWER(centro_costo)) AS centro_costo,
+    CAST(monto AS FLOAT) AS monto,
     fecha
 FROM gastos;
+
+--Cleaning tabla presupuestos
+SELECT
+    TRIM(LOWER(categoria)) AS categoria,
+    TRIM(LOWER(centro_costo)) AS centro_costo,
+    CAST(monto_presupuestado AS FLOAT) AS monto_presupuestado,
+    fecha,
+    version_presupuesto
+FROM presupuesto;
