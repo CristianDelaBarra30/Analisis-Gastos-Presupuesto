ebd25f58c26d6885eb2418c02d8c9cbcc9c56b8b
Cristian De La Barra <cdelabd21@gmail.com>
2026-04-02 01:28:56 -0300

    Update 02_gasto_total.sql

--- sql/02_gasto_total.sql
+++ sql/02_gasto_total.sql
@@ -1,6 +1,13 @@
+WITH clean AS (
+    SELECT
+        TRIM(LOWER(categoria)) AS categoria,
+        monto
+    FROM gastos
+)
+
 SELECT 
     categoria,
     SUM(monto) AS gasto_total
-FROM gastos
+FROM clean
 GROUP BY categoria
 ORDER BY gasto_total DESC;
